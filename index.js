c8ba94086b560ed44ab510926ab30a0252821279
Fabian Beuke <mail@beuke.org>
2018-04-11 23:09:40 +0200

    wss url domain as db collection name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws')
 const MongoClient = require('mongodb').MongoClient
+const { getDomain } = require('tldjs')
 
 const ws = new WebSocket(process.env.WSS_URL)
 const url = 'mongodb://mongodb:27017'
@@ -8,7 +9,8 @@ MongoClient.connect(url, (err, client) => {
   if (err) throw err
   console.log("Connected successfully to server")
   const db = client.db('wss_record')
-  const collection = db.collection(url)
+  const collection_name = getDomain(process.env.WSS_URL)
+  const collection = db.collection(collection_name)
   ws.on('message', data => {
     console.log(data)
     collection.insertMany([ JSON.parse(data) ])
