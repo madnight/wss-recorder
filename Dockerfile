5a34bb686b53c5fa958979d0a5efd7fe2cfba71a
Fabian Beuke <mail@beuke.org>
2018-04-11 22:57:01 +0200

    add nodemon (reload app on file change)

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ RUN mkdir -p /app/src/
 WORKDIR /app/src/
 
 # it takes a moment to boot up mongodb thats why sleep
-CMD sleep 5 && node index.js
+CMD sleep 5 && npm run start
 
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const WebSocket = require('ws')
-
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
 
 const ws = new WebSocket(process.env.WSS_URL)
+const url = 'mongodb://mongodb:27017'
 
-const url = 'mongodb://mongodb:27017';
-const dbName = 'wss_record';
-
-MongoClient.connect(url, function(err, client) {
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-
-  const collection = db.collection('documents');
+MongoClient.connect(url, (err, client) => {
+  if (err) throw err
+  console.log("Connected successfully to server")
+  const db = client.db('wss_record')
+  const collection = db.collection(url)
   ws.on('message', data => {
     console.log(data)
-  collection.insertMany([ JSON.parse(data) ])
+    collection.insertMany([ JSON.parse(data) ])
   })
-});
+})
